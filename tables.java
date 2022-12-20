d6245dfd9ea7527a7e187887258ad921c9034adc
bala <100593516+Balamanikyarao@users.noreply.github.com>
2022-12-20 13:54:48 +0530

    Add files via upload

---
+++ tables.java
@@ -0,0 +1,11 @@
+
+class Tables
+{ public static void main(String[] args)
+ { 
+   int N = 2; 
+   for (int i = 1; i <= 10; i++)
+    { 
+     System.out.println(N + " * " + i + " = " + N * i);
+ } 
+ }
+ }
